1a123d8055da14c2336bb84c722e91488bd8599f
Nicholas Dunn <nexela@gmail.com>
2019-02-27 23:33:20 -0500

    Enable Protected Mode

--- control.lua
+++ control.lua
@@ -3,6 +3,7 @@
 -------------------------------------------------------------------------------
 
 local Event = require('__stdlib__/stdlib/event/event')
+Event.protected_mode = true
 local Player = require('__stdlib__/stdlib/event/player').register_events(true)
 local Area = require('__stdlib__/stdlib/area/area')
 local Position = require('__stdlib__/stdlib/area/position')
