4ce9b4515fb155fbb068adef8617f910c405e932
Clarence "Sparr" Risher <sparr0@gmail.com>
2022-03-05 15:54:13 -0500

    Add setting to ignore collisions (#11)
    * Add setting to ignore collisions
    
    * Add global setting forbidding ignoring collisions

--- settings.lua
+++ settings.lua
@@ -4,5 +4,17 @@ data:extend {
         setting_type = 'runtime-per-user',
         type = 'bool-setting',
         default_value = true
+    },
+    {
+        name = 'dolly-ignore-collisions',
+        setting_type = 'runtime-per-user',
+        type = 'bool-setting',
+        default_value = false
+    },
+    {
+        name = 'dolly-allow-ignore-collisions',
+        setting_type = 'runtime-global',
+        type = 'bool-setting',
+        default_value = false
     }
 }
